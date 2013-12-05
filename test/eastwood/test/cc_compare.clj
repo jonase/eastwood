1cd90a7ad223fcba6c062294e17a47600d6dac88
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-12-04 17:12:45 -0800

    Somewhat easier to see differences between exp and actual test results

---
+++ test/eastwood/test/cc_compare.clj
@@ -0,0 +1,178 @@
+(ns eastwood.test.cc-compare)
+
+;; comparison-class throws exceptions for some types that might be
+;; useful to include.
+
+(defn comparison-class [x]
+  (cond (nil? x) ""
+        ;; Lump all numbers together since Clojure's compare can
+        ;; compare them all to each other sensibly.
+        (number? x) "java.lang.Number"
+
+        ;; sequential? includes lists, conses, vectors, and seqs of
+        ;; just about any collection, although it is recommended not
+        ;; to use this to compare seqs of unordered collections like
+        ;; sets or maps (vectors should be OK).  This should be
+        ;; everything we would want to compare using cmp-seq-lexi
+        ;; below.  TBD: Does it leave anything out?  Include anything
+        ;; it should not?
+        (sequential? x) "clojure.lang.Sequential"
+
+        (set? x) "clojure.lang.IPersistentSet"
+        (map? x) "clojure.lang.IPersistentMap"
+        (.isArray (class x)) "java.util.Arrays"
+
+        ;; Comparable includes Boolean, Character, String, Clojure
+        ;; refs, and many others.
+        (instance? Comparable x) (.getName (class x))
+
+        (or (instance? java.lang.Class x) 
+            (instance? clojure.lang.Var x)) (.getName (class x))
+
+        :else (throw
+               (ex-info (format "cc-cmp does not implement comparison of values with class %s"
+                                (.getName (class x)))
+                        {:value x}))))
+
+
+(defn cmp-seq-lexi
+  "Compare sequences x and y in lexicographic order, using comparator
+cmpf to compare elements from x and y to each other.  As a comparator
+function, cmp-seq-lexi returns a negative, 0, or positive integer if x
+is less than, equal to, or greater than y."
+  [cmpf x y]
+  (loop [x x
+         y y]
+    (if (seq x)
+      (if (seq y)
+        (let [c (cmpf (first x) (first y))]
+          (if (zero? c)
+            (recur (rest x) (rest y))
+            c))
+        ;; else we reached end of y first, so x > y
+        1)
+      (if (seq y)
+        ;; we reached end of x first, so x < y
+        -1
+        ;; Sequences contain same elements.  x = y
+        0))))
+
+
+;; The same result can be obtained by calling cmp-seq-lexi on two
+;; vectors, but this one should allocate less memory comparing
+;; vectors.
+(defn cmp-vec-lexi
+  "Compare vectors x and y in lexicographic order, using comparator
+cmpf to compare elements from x and y to each other.  As a comparator
+function, cmp-seq-lexi returns a negative, 0, or positive integer if x
+is less than, equal to, or greater than y.
+
+It is possible to use cmp-seq-lexi on vectors, too, but this function
+should be faster and allocate less memory than cmp-seq-lexi, but only
+works for vectors."
+  [cmpf x y]
+  (let [x-len (count x)
+        y-len (count y)
+        len (min x-len y-len)]
+    (loop [i 0]
+      (if (== i len)
+        ;; If all elements 0..(len-1) are same, shorter vector comes
+        ;; first.
+        (compare x-len y-len) 
+        (let [c (cmpf (x i) (y i))]
+          (if (zero? c)
+            (recur (inc i))
+            c))))))
+
+(defn cmp-array-lexi
+  "Compare Java arrays x and y in lexicographic order, using
+comparator cmpf to compare elements from x and y to each other.  As a
+comparator function, cmp-array-lexi returns a negative, 0, or positive
+integer if x is less than, equal to, or greater than y."
+  [cmpf x y]
+  (let [x-len (alength x)
+        y-len (alength y)
+        len (min x-len y-len)]
+    (loop [i 0]
+      (if (== i len)
+        ;; If all elements 0..(len-1) are same, shorter array comes
+        ;; first.
+        (compare x-len y-len) 
+        (let [c (cmpf (aget x i) (aget y i))]
+          (if (zero? c)
+            (recur (inc i))
+            c))))))
+
+
+(defn cc-cmp
+  "cc-cmp compares two values x and y.  As a comparator, it returns a
+negative, 0, or positive integer if x is less than, equal to, or
+greater than y.
+
+cc-cmp can compare values of many types, including numbers, strings,
+symbols, keywords, vectors, lists, sequences, sets, maps, records,
+Java arrays, anything that implements the Java Comparable
+interface (including booleans, characters, File, URL, UUID, Clojure
+refs), and nil.
+
+Unlike the function compare, it sorts vectors in lexicographic order.
+It also sorts lists, sequences, and Java arrays in lexicographic
+order, on which compare throws exceptions.
+
+Also unlike compare, cc-cmp can compare values of different types to
+each other without throwing an exception, if it can compare them at
+all.  Values of different types are sorted relative to each other by a
+string that is the name of their class.  Note that all numbers use the
+string \"java.lang.Number\" so that numbers are sorted together,
+rather than separated out by type, and for a similar reason all
+vectors, lists, and sequences use the string
+\"clojure.lang.Sequential\".  All sets use the string
+\"clojure.lang.IPersistentSet\", and all maps and records use the
+string \"clojure.lang.IPersistentMap\".  All Java arrays use the
+string \"java.util.Arrays\" (that is not the name of any instantiable
+class -- it is simply used for sorting Java arrays versus other
+objects).
+
+cc-cmp throws an exception if given any type of value not mentioned
+above, e.g. Java arrays."
+
+  [x y]
+  (let [x-cls (comparison-class x)
+        y-cls (comparison-class y)
+        c (compare x-cls y-cls)]
+    (cond (not= c 0) c  ; different classes
+
+          ;; Compare Class instances by their names as converted to
+          ;; strings.
+          (#{"java.lang.Class" "clojure.lang.Var"} x-cls)
+          (compare (str x) (str y))
+
+          ;; Compare sets to each other as sequences, with elements in
+          ;; sorted order.
+          (= x-cls "clojure.lang.IPersistentSet")
+          (cmp-seq-lexi cc-cmp (sort cc-cmp x) (sort cc-cmp y))
+
+          ;; Compare maps to each other as sequences of [key val]
+          ;; pairs, with pairs in order sorted by key.
+          (= x-cls "clojure.lang.IPersistentMap")
+          (cmp-seq-lexi cc-cmp
+                        (sort-by key cc-cmp (seq x))
+                        (sort-by key cc-cmp (seq y)))
+
+          (= x-cls "java.util.Arrays")
+          (cmp-array-lexi cc-cmp x y)
+
+          ;; Make a special check for two vectors, since cmp-vec-lexi
+          ;; should allocate less memory comparing them than
+          ;; cmp-seq-lexi.  Both here and for comparing sequences, we
+          ;; must use cc-cmp recursively on the elements, because if
+          ;; we used compare we would lose the ability to compare
+          ;; elements with different types.
+          (and (vector? x) (vector? y)) (cmp-vec-lexi cc-cmp x y)
+
+          ;; This will compare any two sequences, if they are not both
+          ;; vectors, e.g. a vector and a list will be compared here.
+          (= x-cls "clojure.lang.Sequential")
+          (cmp-seq-lexi cc-cmp x y)
+          
+          :else (compare x y))))
--- test/eastwood/test/linters_test.clj
+++ test/eastwood/test/linters_test.clj
@@ -1,118 +1,132 @@
 (ns eastwood.test.linters-test
   (:use [clojure.test])
-  (:use [eastwood.core]))
+  (:use [eastwood.core])
+  (:require [clojure.data :as data]
+            [eastwood.test.cc-compare :as ccmp]))
+
+(defn to-sorted-map [c]
+  (if (map? c)
+    (into (sorted-map-by ccmp/cc-cmp) c)
+    c))
+
+(defn make-sorted [c]
+  (clojure.walk/postwalk to-sorted-map c))
+
+(defmacro lint-test [ns-sym linters opts expected-lint-result]
+  `(is (= (make-sorted (data/diff
+                        (frequencies (lint-ns-noprint ~ns-sym ~linters ~opts))
+                        ~expected-lint-result))
+          (make-sorted [nil nil ~expected-lint-result]))))
 
 
 (deftest test1
-  (is (= (frequencies (lint-ns-noprint
-                       'eastwood.test.testcases.f01
-                       [:misplaced-docstrings :def-in-def :redefd-vars]
-                       {}))
-         {
-          {:linter :redefd-vars,
-           :msg
-           "Var #'eastwood.test.testcases.f01/i-am-redefd def'd 2 times at lines: 4 5",
-           :line 5}
-          1,
-          {:linter :misplaced-docstrings,
-           :msg "Possibly misplaced docstring, #'eastwood.test.testcases.f01/foo2",
-           :line 12}
-          1,
-          {:linter :misplaced-docstrings,
-           :msg "Possibly misplaced docstring, #'eastwood.test.testcases.f01/foo5",
-           :line 31}
-          1,
-          {:linter :redefd-vars,
-           :msg "Var #'eastwood.test.testcases.f01/test1-redefd def'd 3 times at lines: 42 46 49",
-           :line 46}
-          1,
-          {:linter :redefd-vars,
-           :msg "Var #'eastwood.test.testcases.f01/i-am-redefd2 def'd 2 times at lines: 70 73",
-           :line 73}
-          1,
-          {:linter :def-in-def,
-           :msg "There is a def of #'eastwood.test.testcases.f01/def-in-def1 nested inside def TBD",
-           :line 82}
-          1,
-          }))
+  (lint-test
+   'eastwood.test.testcases.f01
+   [:misplaced-docstrings :def-in-def :redefd-vars]
+   {}
+   {
+    {:linter :redefd-vars,
+     :msg
+     "Var #'eastwood.test.testcases.f01/i-am-redefd def'd 2 times at lines: 4 5",
+     :line 5}
+    1,
+    {:linter :misplaced-docstrings,
+     :msg "Possibly misplaced docstring, #'eastwood.test.testcases.f01/foo2",
+     :line 12}
+    1,
+    {:linter :misplaced-docstrings,
+     :msg "Possibly misplaced docstring, #'eastwood.test.testcases.f01/foo5",
+     :line 31}
+    1,
+    {:linter :redefd-vars,
+     :msg "Var #'eastwood.test.testcases.f01/test1-redefd def'd 3 times at lines: 42 46 49",
+     :line 46}
+    1,
+    {:linter :redefd-vars,
+     :msg "Var #'eastwood.test.testcases.f01/i-am-redefd2 def'd 2 times at lines: 70 73",
+     :line 73}
+    1,
+    {:linter :def-in-def,
+     :msg "There is a def of #'eastwood.test.testcases.f01/def-in-def1 nested inside def TBD",
+     :line 82}
+    1,
+    })
 
   ;; TBD: I do not know why the i-am-inner-defonce-sym warning appears
   ;; twice in the result.  Once would be enough.
-  (is (= (frequencies (lint-ns-noprint
-                       'eastwood.test.testcases.f02
-                       [:misplaced-docstrings :def-in-def :redefd-vars]
-                       {}))
-         {
-          {:linter :redefd-vars,
-           :msg "Var #'eastwood.test.testcases.f02/i-am-defonced-and-defmultid def'd 2 times at lines:  ",
-           :line nil}
-          1,
-          {:linter :redefd-vars,
-           :msg "Var #'eastwood.test.testcases.f02/i-am-a-redefd-defmulti def'd 2 times at lines:  ",
-           :line nil}
-          1,
-          {:linter :def-in-def,
-           :msg "There is a def of #'eastwood.test.testcases.f02/i-am-inner-defonce-sym nested inside def TBD",
-           :line nil}
-          2,
-          }
-         ))
+  (lint-test
+   'eastwood.test.testcases.f02
+   [:misplaced-docstrings :def-in-def :redefd-vars]
+   {}
+   {
+    {:linter :redefd-vars,
+     :msg "Var #'eastwood.test.testcases.f02/i-am-defonced-and-defmultid def'd 2 times at lines:  ",
+     :line nil}
+    1,
+    {:linter :redefd-vars,
+     :msg "Var #'eastwood.test.testcases.f02/i-am-a-redefd-defmulti def'd 2 times at lines:  ",
+     :line nil}
+    1,
+    {:linter :def-in-def,
+     :msg "There is a def of #'eastwood.test.testcases.f02/i-am-inner-defonce-sym nested inside def TBD",
+     :line nil}
+    2,
+    }
+   )
 
-  (is (= (frequencies (lint-ns-noprint
-                       'eastwood.test.testcases.f03
-                       [:misplaced-docstrings :def-in-def :redefd-vars]
-                       {}))
-         {}))
-  (is (= (frequencies (lint-ns-noprint
-                       'eastwood.test.testcases.f04
-                       [:misplaced-docstrings :def-in-def :redefd-vars]
-                       {}))
-         {}))
+  (lint-test
+   'eastwood.test.testcases.f03
+   [:misplaced-docstrings :def-in-def :redefd-vars]
+   {}
+   {})
+  (lint-test
+   'eastwood.test.testcases.f04
+   [:misplaced-docstrings :def-in-def :redefd-vars]
+   {}
+   {})
   ;; The following test is known to fail with Clojure 1.5.1 because of
   ;; protocol method names that begin with "-".  See
   ;; http://dev.clojure.org/jira/browse/TANAL-17 and
   ;; http://dev.clojure.org/jira/browse/CLJ-1202
   (when (and (>= (:major *clojure-version*) 1)
              (>= (:minor *clojure-version*) 6))
-    (is (= (frequencies (lint-ns-noprint
-                         'eastwood.test.testcases.f05
-                         [:misplaced-docstrings :def-in-def :redefd-vars]
-                         {}))
-           {})))
-  (is (= (frequencies (lint-ns-noprint
-                       'eastwood.test.testcases.f06
-                       [:unused-fn-args :misplaced-docstrings :def-in-def
-                        :redefd-vars]
-                       {}))
-         {
-          {:linter :unused-fn-args,
-           :msg "Function args [y (line 5)] of (or within) fn-with-unused-args are never used",
-           :line 5}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [y (line 9)] of (or within) fn-with-unused-args2 are never used",
-           :line 9}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [w (line 20)] of (or within) fn-with-unused-args3 are never used",
-           :line 19}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [body (line 33)] of (or within) macro2 are never used",
-           :line 33}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [z (line 50)] of (or within) fn-with-unused-args4 are never used",
-           :line 50}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [val (line 69) f (line 68)] of (or within) protocol CollReduce type clojure.lang.ASeq method coll-reduce are never used",
-           :line nil}
-          1,
-          {:linter :unused-fn-args,
-           :msg "Function args [f (line 64) coll (line 63)] of (or within) protocol CollReduce type nil method coll-reduce are never used",
-           :line nil}
-          1,
-          }
-         ))
+    (lint-test
+     'eastwood.test.testcases.f05
+     [:misplaced-docstrings :def-in-def :redefd-vars]
+     {}
+     {}))
+  (lint-test
+   'eastwood.test.testcases.f06
+   [:unused-fn-args :misplaced-docstrings :def-in-def :redefd-vars]
+   {}
+   {
+    {:linter :unused-fn-args,
+     :msg "Function args [y (line 5)] of (or within) fn-with-unused-args are never used",
+     :line 5}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [y (line 9)] of (or within) fn-with-unused-args2 are never used",
+     :line 9}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [w (line 20)] of (or within) fn-with-unused-args3 are never used",
+     :line 19}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [body (line 33)] of (or within) macro2 are never used",
+     :line 33}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [z (line 50)] of (or within) fn-with-unused-args4 are never used",
+     :line 50}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [val (line 69) f (line 68)] of (or within) protocol CollReduce type clojure.lang.ASeq method coll-reduce are never used",
+     :line nil}
+    1,
+    {:linter :unused-fn-args,
+     :msg "Function args [f (line 64) coll (line 63)] of (or within) protocol CollReduce type nil method coll-reduce are never used",
+     :line nil}
+    1,
+    })
   )
