0518444895f2b7baed3c7d1f78ac1a7e9d52fe0a
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-12-16 03:29:03 -0800

    Update a couple of project.clj files in the crucible

--- crucible/project-clj-files/buffy/project.clj
+++ crucible/project-clj-files/buffy/project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/buffy "0.6.0-SNAPSHOT"
+(defproject clojurewerkz/buffy "1.0.0-beta2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- crucible/project-clj-files/cheshire/project.clj
+++ crucible/project-clj-files/cheshire/project.clj
@@ -13,6 +13,7 @@
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :benchmark {:test-paths ["benchmarks"]
                          :dependencies [[criterium "0.4.2"]
                                         [org.clojure/data.json "0.2.3"]
