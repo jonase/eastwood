3ab191c7da0f5675012d7a343d8e27d23ebf196c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-12-16 22:03:19 -0800

    Add Leiningen profile for Clojure 1.6 for criterium

--- crucible/project-clj-files/criterium/project.clj
+++ crucible/project-clj-files/criterium/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:hugoduncan/criterium.git"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
   :local-repo-classpath true)
