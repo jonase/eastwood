069492d4c9558814bb4dbe75a2b25a137e2ea02d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-12-23 22:55:47 -0800

    Try to make exception thrown by :suspicious-test linter a bit more informative

--- src/eastwood/linters/typos.clj
+++ src/eastwood/linters/typos.clj
@@ -227,15 +227,21 @@
 (defn suspicious-test [{:keys [asts source]}]
   (binding [*var-info-map* (edn/read-string (slurp (io/resource "var-info.edn")))]
     (doall
-     (let [this-ns (-> (first asts) :env :ns the-ns)
-           forms (util/string->forms source this-ns true)
-           is-forms (subforms-with-first-symbol forms 'is)
-           deftest-forms (subforms-with-first-symbol forms 'deftest)
-           testing-forms (subforms-with-first-symbol forms 'testing)
-           deftest-subexprs (apply concat
-                                   (map #(nthnext % 2) deftest-forms))
-           testing-subexprs (apply concat
-                                   (map #(nthnext % 2) testing-forms))]
-       (concat (suspicious-is-forms is-forms)
-               (predicate-forms deftest-subexprs 'deftest)
-               (predicate-forms testing-subexprs 'testing))))))
+     (let [this-ns-sym (-> (first asts) :env :ns)
+           this-ns (try (the-ns this-ns-sym)
+                        (catch Exception e nil))]
+       (if (nil? this-ns)
+         (throw (ex-info (format "Could not find namespace '%s'" this-ns-sym)
+                         {:missing-namespace this-ns-sym,
+                          :ast (first asts)}))
+         (let [forms (util/string->forms source this-ns true)
+               is-forms (subforms-with-first-symbol forms 'is)
+               deftest-forms (subforms-with-first-symbol forms 'deftest)
+               testing-forms (subforms-with-first-symbol forms 'testing)
+               deftest-subexprs (apply concat
+                                       (map #(nthnext % 2) deftest-forms))
+               testing-subexprs (apply concat
+                                       (map #(nthnext % 2) testing-forms))]
+           (concat (suspicious-is-forms is-forms)
+                   (predicate-forms deftest-subexprs 'deftest)
+                   (predicate-forms testing-subexprs 'testing))))))))
