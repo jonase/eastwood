9f33b871460e3fec57e3e8a7683e378e6f6bbecd
Nicola Mometto <brobronsa@gmail.com>
2014-03-26 01:15:00 +0100

    update clojure dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jonase/eastwood "0.1.2-SNAPSHOT"
   :description "A Clojure lint tool"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.analyzer "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.analyzer.jvm "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.namespace "0.2.4"]
@@ -9,7 +9,7 @@
                  [leinjacker "0.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]]
                    :source-paths [ "cases" ]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   ;; Note: comment out the following line if you want to do 'lein
   ;; test' and get a Clojure version later than 1.5.1, even if you
   ;; have an explicit org.clojure/clojure in the :dependencies
