69a75d74a6ebab75d49e28eadf247359d17512f2
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-09-18 17:00:48 -0400

    Add Dolly snapshot version to dev deps

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "4.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]
+                                  [jafingerhut/dolly "0.1.0-SNAPSHOT"]]
                    :source-paths [ "cases" ]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
