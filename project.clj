559421ae357a1b4a748ab7bf387f1ddb56e06625
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-10-02 18:18:33 -0700

    Update dolly dep to first released version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "4.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]
-                                  [jafingerhut/dolly "0.1.0-SNAPSHOT"]]
+                                  [jafingerhut/dolly "0.1.0"]]
                    :source-paths [ "cases" ]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
