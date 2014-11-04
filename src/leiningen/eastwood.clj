e3940911ca32d94be5a3e0401d1a9d61743a9cec
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-11-04 13:39:17 -0800

    Change versions to 0.2.0-SNAPSHOT for future development

--- src/leiningen/eastwood.clj
+++ src/leiningen/eastwood.clj
@@ -2,7 +2,7 @@
   (:require [eastwood.copieddeps.dep6.leinjacker.eval :as leval]
             [eastwood.copieddeps.dep6.leinjacker.deps :refer [add-if-missing]]))
 
-(def eastwood-version-string "0.1.5")
+(def eastwood-version-string "0.2.0-SNAPSHOT")
 
 (defn help []
   "Lint your Clojure code.")
