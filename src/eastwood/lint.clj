dc8dc7c5e749088c1caba3281d4695bfc01725a9
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-11-13 20:51:00 -0800

    Change versions back to 0.2.0-SNAPSHOT for further development

--- src/eastwood/lint.clj
+++ src/eastwood/lint.clj
@@ -22,7 +22,7 @@
 (def eastwood-url "https://github.com/jonase/eastwood")
 
 (def ^:dynamic *eastwood-version*
-  {:major 0, :minor 2, :incremental 0, :qualifier "alpha1"})
+  {:major 0, :minor 2, :incremental 0, :qualifier "SNAPSHOT"})
 
 (defn eastwood-version []
   (let [{:keys [major minor incremental qualifier]} *eastwood-version*]
