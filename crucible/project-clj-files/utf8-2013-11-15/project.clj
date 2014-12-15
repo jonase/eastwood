3855f22f08597ee27638ba22f1ba6881e40df6cd
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-15 15:39:45 -0800

    In crucible, update utf8 dep on nio to fix bug exposed with Clojure 1.7
    Nicola Mometto analyzed this bug and found it was due to utf8
    depending on an older version of the nio library that had a bug only
    exposed by Clojure 1.7.0-alpha3:
    
        http://dev.clojure.org/jira/browse/TANAL-97

--- crucible/project-clj-files/utf8-2013-11-15/project.clj
+++ crucible/project-clj-files/utf8-2013-11-15/project.clj
@@ -5,6 +5,6 @@
             :url "http://mozilla.org/MPL/2.0/"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [nio "1.0.1"]]
+                 [nio "1.0.3"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}})
