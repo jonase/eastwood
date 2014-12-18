d16024a26be3b324e8ba3f90fdf2035c9b50d1a5
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-18 00:23:52 -0800

    Back to 0.2.1-SNAPSHOT for further development

--- src/eastwood/lint.clj
+++ src/eastwood/lint.clj
@@ -21,7 +21,7 @@
 (def eastwood-url "https://github.com/jonase/eastwood")
 
 (def ^:dynamic *eastwood-version*
-  {:major 0, :minor 2, :incremental 1, :qualifier "alpha2"})
+  {:major 0, :minor 2, :incremental 1, :qualifier "SNAPSHOT"})
 
 (defn eastwood-version []
   (let [{:keys [major minor incremental qualifier]} *eastwood-version*]
