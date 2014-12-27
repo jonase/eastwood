11fc76af2eee711a35d72fcd88c884f8fcab3a8a
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-27 11:18:15 -0800

    Update version of core.memoize used in crucible to Nov 16 2014

--- crucible/project-clj-files/core.memoize-2014-11-16/project.clj
+++ crucible/project-clj-files/core.memoize-2014-11-16/project.clj
@@ -1,14 +1,14 @@
 (defproject org.clojure/core.memoize "0.5.7-SNAPSHOT"
   :description "A memoization library for Clojure."
-  :global-vars {*warn-on-reflection* true}
   :dependencies [;;[org.clojure/pom.contrib "0.1.2"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.6.3"]]
+                 [org.clojure/core.cache "0.6.4"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :plugins [[lein-swank "1.4.4"]
             [lein-marginalia "0.7.1"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :source-paths ["src/main/clojure"]
-  :test-paths   ["src/test/clojure"])
+  :test-paths   ["src/test/clojure"]
+  :global-vars {*warn-on-reflection* true})
 
