4a6e7d306f80f1e48c4e260881eeffa71bb86fbf
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-27 23:25:59 -0800

    Update version of buffy used in crucible to Dec 20 2014

---
+++ crucible/project-clj-files/buffy-2014-12-20/project.clj
@@ -0,0 +1,23 @@
+(defproject clojurewerkz/buffy "1.1.0-SNAPSHOT"
+  :description "Clojure library for working with binary data"
+  :url "http://github.com/clojurewerkz/buffy"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.clojure/clojure     "1.6.0"]
+                 [io.netty/netty-buffer   "4.0.24.Final"]
+                 [org.clojure/test.check  "0.6.2"]]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :dev {:resource-paths ["test/resources"]
+                   :plugins [[codox "0.8.10"]]
+                   :codox {:sources ["src/clojure"]
+                           :output-dir "doc/api"}}}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev:dev,master"]})
