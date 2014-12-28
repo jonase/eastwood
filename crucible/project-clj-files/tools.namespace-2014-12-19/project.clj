a3dfbbcc5c3d6d0b3c1c782d25f7740808e8efee
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-27 22:36:05 -0800

    Update version of tools.namespace used in crucible to Dec 19 2014

--- crucible/project-clj-files/tools.namespace-2014-12-19/project.clj
+++ crucible/project-clj-files/tools.namespace-2014-12-19/project.clj
@@ -1,10 +1,8 @@
-(defproject org.clojure/tools.namespace "0.2.5-SNAPSHOT"
+(defproject org.clojure/tools.namespace "0.2.9-SNAPSHOT"
   :description "tools.namespace 0.2.5-SNAPSHOT"
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :source-paths [ "src/main/clojure" ]
   :test-paths [ "src/test/clojure" ]
