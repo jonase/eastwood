c11f2b0fd29420efce170973b08856479a005fd6
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-27 23:20:46 -0800

    Update version of automat used in crucible to Nov 24 2014

--- crucible/project-clj-files/automat-2014-11-24/project.clj
+++ crucible/project-clj-files/automat-2014-11-24/project.clj
@@ -1,15 +1,15 @@
-(defproject automat "0.1.0-SNAPSHOT"
+(defproject automat "0.1.4-SNAPSHOT"
   :description ""
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[rhizome "0.1.9"]
-                 [primitive-math "0.1.3"]
-                 [potemkin "0.3.4"]
-                 [riddley "0.1.7-SNAPSHOT"]
+  :dependencies [[rhizome "0.2.1"]
+                 [primitive-math "0.1.4"]
+                 [potemkin "0.3.9"]
+                 [riddley "0.1.7"]
                  [proteus "0.1.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [reiddraper/simple-check "0.5.2"]
-                                  [criterium "0.4.2"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [org.clojure/test.check "0.5.9"]
+                                  [criterium "0.4.3"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
@@ -22,4 +22,4 @@
   :javac-options ["-target" "1.5" "-source" "1.5"]
   :plugins [[codox "0.6.4"]]
   :codox {:writer codox-md.writer/write-docs
-          :include [automat.core]})
+          :include [automat.core automat.viz]})
