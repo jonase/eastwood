c24be686f24e475d456f700ff73cf6c054df882d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-12-29 15:43:35 -0800

    Add byte-streams library to crucible projects

---
+++ crucible/project-clj-files/byte-streams-2014-11-26/project.clj
@@ -0,0 +1,21 @@
+(defproject byte-streams "0.2.0-alpha7"
+  :description "A simple way to handle the menagerie of Java byte represenations."
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[primitive-math "0.1.4"]
+                 [clj-tuple "0.1.7"]
+                 [manifold "0.1.0-beta1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                                  [reiddraper/simple-check "0.5.6"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
+  :test-selectors {:stress :stress
+                   :default (complement :stress)}
+  :plugins [[codox "0.6.4"]]
+  :codox {:writer codox-md.writer/write-docs
+          :include [byte-streams]}
+  :global-vars {*warn-on-reflection* true}
+  :java-source-paths ["src"]
+  :javac-options ["-target" "1.5" "-source" "1.5"]
+  :jvm-opts ^:replace ["-server" "-Xmx4g"])
