86fb83f2e13b2ad0ddd7831630012523f850b542
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-01-10 16:13:11 -0800

    Add :1.6 profile to buffy project, like other crucible projects have

--- crucible/project-clj-files/buffy-2014-12-20/project.clj
+++ crucible/project-clj-files/buffy-2014-12-20/project.clj
@@ -4,10 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure     "1.6.0"]
+  :dependencies [[org.clojure/clojure     "1.5.1"]
                  [io.netty/netty-buffer   "4.0.24.Final"]
                  [org.clojure/test.check  "0.6.2"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
