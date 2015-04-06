9743e2945d6e3eeb2807117ae5b40e1f513fa7e0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-04-06 12:47:38 -0700

    Upgrade compojure version in crucible to one compatible with 1.7.0-alpha6

--- crucible/project-clj-files/compojure-2015-04-01/project.clj
+++ crucible/project-clj-files/compojure-2015-04-01/project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "1.2.1"
+(defproject compojure "1.3.3"
   :description "A concise routing library for Ring"
   :url "https://github.com/weavejester/compojure"
   :license {:name "Eclipse Public License"
@@ -6,15 +6,17 @@
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [clout "2.0.0"]
-                 [medley "0.5.3"]
-                 [ring/ring-core "1.3.1"]
+                 [clout "2.1.1"]
+                 [medley "0.5.5"]
+                 [ring/ring-core "1.3.2"]
                  [ring/ring-codec "1.0.0"]]
   :plugins [[codox "0.8.10"]]
-  :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.2.1/"
+  :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.3.3/"
           :src-linenum-anchor-prefix "L"}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]
+  {:dev {:jvm-opts ^:replace []
+         :dependencies [[ring/ring-mock "0.2.0"]
+                        [criterium "0.4.3"]
                         [javax.servlet/servlet-api "2.5"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}})
