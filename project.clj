c518f8d2d44d2d8d8950951ec5e26fb4de645ef9
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2016-01-15 01:29:00 -0800

    Update Clojure 1.8.0 dep to latest, 1.8.0-RC5

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC1"]]}}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC5"]]}}
   ;; Note: comment out the following line if you want to do 'lein
   ;; test' and get a Clojure version later than 1.5.1, even if you
   ;; have an explicit org.clojure/clojure in the :dependencies
