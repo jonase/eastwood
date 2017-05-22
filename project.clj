7ba5b2d0736df8886aeb575ef12b7bd9f7ef0e96
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-05-21 19:30:23 -0700

    Update Clojure 1.8 and 1.9 deps in project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/clojure "1.5.1"]
                  ;[org.clojure/clojure "1.6.0"]
                  ;[org.clojure/clojure "1.7.0"]
-                 ;[org.clojure/clojure "1.8.0-RC1"]
+                 ;[org.clojure/clojure "1.8.0"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "4.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]
@@ -32,7 +32,8 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC5"]]}}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha16"]]}}
   ;; Note: comment out the following line if you want to do 'lein
   ;; test' and get a Clojure version later than 1.5.1, even if you
   ;; have an explicit org.clojure/clojure in the :dependencies
