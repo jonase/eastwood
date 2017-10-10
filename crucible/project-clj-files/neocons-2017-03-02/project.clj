0a38476b6f63b795c9f7d8a8ec6fe9d839e65605
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-10 03:46:26 -0700

    Update neocons in crucible to a version that does not fail Clojure 1.9 spec checks

--- crucible/project-clj-files/neocons-2017-03-02/project.clj
+++ crucible/project-clj-files/neocons-2017-03-02/project.clj
@@ -1,12 +1,14 @@
-(defproject clojurewerkz/neocons "3.2.0-SNAPSHOT"
-  :description "Neocons is a feature rich idiomatic Clojure client for the Neo4J REST API"
+(defproject clojurewerkz/neocons "3.2.0"
+  :description "Neocons is a feature rich idiomatic Clojure client for the Neo4J REST API. It also supports Bolt protocol."
   :url "http://clojureneo4j.info"
   :license {:name "Eclipse Public License"}
   :min-lein-version "2.5.1"
-  :dependencies [[org.clojure/clojure  "1.7.0"]
-                 [cheshire             "5.5.0"]
-                 [clj-http             "2.0.0" :exclusions [org.clojure/clojure]]
-                 [clojurewerkz/support "1.1.0" :exclusions [com.google.guava/guava]]]
+  :dependencies [[org.clojure/clojure  "1.8.0"]
+                 [cheshire             "5.6.3"]
+                 [clj-http             "3.3.0" :exclusions [org.clojure/clojure]]
+                 [clojurewerkz/support "1.1.0" :exclusions [com.google.guava/guava]]
+                 [org.neo4j.driver/neo4j-java-driver "1.0.6"]]
+
   :test-selectors {:default        (fn [m] (and (not (:time-consuming m))
                                                 (not (:http-auth m))
                                                 (not (:edge-features m))
@@ -38,17 +40,17 @@
                    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
                    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
                    :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
-                   :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
-                   :dev {:plugins [[codox "0.8.10"]]
-                         :codox {:sources ["src/clojure"]
-                                 :output-dir "doc/api"}}
+                   :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+                   :dev {:plugins [[lein-codox "0.9.0"]]
+                         :codox {:source-paths ["src/clojure"]}}
                    ;; this version of clj-http depends on HTTPCore 4.2.x which
                    ;; some projects (e.g. using Spring's RestTemplate) can rely on,
                    ;; so we test for compatibility with it. MK.
                    :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}}
   :codox {:src-dir-uri "https://github.com/michaelklishin/neocons/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :aliases        {"all" ["with-profile" "dev:dev,1.5:dev,1.6:dev,master:dev,cljhttp076:dev,1.6,cljhttp076"]}
+
+  :aliases        {"all" ["with-profile" "dev:dev,1.7:dev,master:dev,cljhttp076"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
