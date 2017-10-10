92e1ed2e90c1665ed00f4df7542a63036872f05a
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-10 00:10:42 -0700

    Update tools.analyzer, tools.analyzer.jvm and tools.reader in crucible

--- crucible/project-clj-files/tools.analyzer-2017-03-23/project.clj
+++ crucible/project-clj-files/tools.analyzer-2017-03-23/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.analyzer "0.6.5-SNAPSHOT"
+(defproject org.clojure/tools.analyzer "0.6.8-SNAPSHOT"
   :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
   :url "https://github.com/clojure/tools.analyzer"
   :license {:name "Eclipse Public License"
@@ -11,5 +11,7 @@
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
              }
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.5067" :scope "provided" :exclusions [joda-time]]])
+  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+                 [com.datomic/datomic-free "0.9.5327" :scope "provided" :exclusions [joda-time]]]
+  :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
+                 ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]])
--- crucible/project-clj-files/tools.analyzer.jvm-2017-07-19/project.clj
+++ crucible/project-clj-files/tools.analyzer.jvm-2017-07-19/project.clj
@@ -1,18 +1,20 @@
-(defproject org.clojure/tools.analyzer.jvm "0.6.5-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.7.1-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+                 [org.clojure/core.memoize "0.5.9"]
+                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [org.clojure/tools.analyzer "0.6.9"]
+                 [org.ow2.asm/asm "5.2"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
              }
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.memoize "0.5.6"]
-                 [org.clojure/tools.reader "0.8.12"]
-                 [org.clojure/tools.analyzer "0.6.4"]
-                 [org.ow2.asm/asm-all "4.2"]])
+  :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
+                 ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]])
