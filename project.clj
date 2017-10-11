e3b2d934db13323eb49927cfb27803aa1909d9e3
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-11 13:57:59 -0700

    Tweaks in project.clj

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
                  ;[org.clojure/clojure "1.5.1"]
                  ;[org.clojure/clojure "1.6.0"]
                  ;[org.clojure/clojure "1.7.0"]
-                 ;[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojure "1.8.0"]
+                 ;[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "4.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.2"]
@@ -34,7 +34,7 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha16"]]}}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
   ;; Note: comment out the following line if you want to do 'lein
   ;; test' and get a Clojure version later than 1.5.1, even if you
   ;; have an explicit org.clojure/clojure in the :dependencies
