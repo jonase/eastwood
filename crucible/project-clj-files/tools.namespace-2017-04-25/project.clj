a84743c642bdfe289639bcc387f8494da6db6354
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-12 10:44:37 -0700

    Update tools.namespace in crucible to most recent version

---
+++ crucible/project-clj-files/tools.namespace-2017-04-25/project.clj
@@ -0,0 +1,13 @@
+(defproject org.clojure/tools.namespace "0.3.0-SNAPSHOT"
+  :description "tools.namespace 0.3.0-SNAPSHOT"
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.classpath "0.2.3"]
+                 [org.clojure/tools.reader "0.10.0"]]
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+             }
+  :source-paths [ "src/main/clojure" ]
+  :test-paths [ "src/test/clojure" ]
+  )
