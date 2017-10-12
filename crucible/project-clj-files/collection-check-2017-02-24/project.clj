5e7f6912b3da7502a16b387798690c4670fb0278
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-12 09:48:53 -0700

    Update collection-check project in crucible to recent version

--- crucible/project-clj-files/collection-check-2017-02-24/project.clj
+++ crucible/project-clj-files/collection-check-2017-02-24/project.clj
@@ -1,11 +1,10 @@
-(defproject collection-check "0.1.4"
+(defproject collection-check "0.1.7"
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/test.check "0.5.9"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :dependencies [[org.clojure/test.check "0.8.0"]
+                 [com.gfredericks/test.chuck "0.1.21"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
