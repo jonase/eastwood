c18e339d34f7bfeb7f14e469d22124b778f6ddf5
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-10-13 21:55:31 -0700

    Remove patch no longer needed for core.async project in crucible
    now that it has been updated to a more recent version that includes
    the change in that patch.

--- crucible/project-clj-files/core.async-2017-05-26/modify.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-script_fullpath_dir="$1"
-clone_fullpath_dir="$2"
-
-echo "Apply core.async patch for ticket ASYNC-54 ..."
-
-cd "${clone_fullpath_dir}"
-patch -p1 < "${script_fullpath_dir}/0001-Fix-MAX-QUEUE-SIZE-type-hint-def-evaluates-the-metad.patch"
