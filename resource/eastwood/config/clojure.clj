2235d8b4d5da8ccc9ec47a96257eee05b9f25c00
Erik Assum <erik@assum.net>
2018-05-06 22:50:12 +0200

    [Fix #253] Disable warning when using s/keys with only :opt-un (#254)

--- resource/eastwood/config/clojure.clj
+++ resource/eastwood/config/clojure.clj
@@ -36,9 +36,10 @@
   {:linter :suspicious-expression
    :for-macro 'clojure.core/and
    :if-inside-macroexpansion-of #{'clojure.spec/every 'clojure.spec.alpha/every
-                                  'clojure.spec/and 'clojure.spec.alpha/and}
+                                  'clojure.spec/and 'clojure.spec.alpha/and
+                                  'clojure.spec/keys 'clojure.spec.alpha/keys}
    :within-depth 6
-   :reason "clojure.spec's macros `every` and `and` often contain `clojure.core/and` invocations with only one argument."})
+   :reason "clojure.spec's macros `keys`, `every`, and `and` often contain `clojure.core/and` invocations with only one argument."})
 
 (disable-warning
  {:linter :constant-test
