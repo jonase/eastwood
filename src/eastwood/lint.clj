7aac814ab4b9406cdd710d3ad9a8c974b3ff3681
Erik Assum <erik@assum.net>
2018-05-13 21:15:48 +0200

    Prepare for next version (#261)

--- src/eastwood/lint.clj
+++ src/eastwood/lint.clj
@@ -23,7 +23,7 @@
 (def eastwood-url "https://github.com/jonase/eastwood")
 
 (def ^:dynamic *eastwood-version*
-  {:major 0, :minor 2, :incremental 6})
+  {:major 0, :minor 2, :incremental 7, :qualifier "SNAPSHOT"})
 
 (defn eastwood-version []
   (let [{:keys [major minor incremental qualifier]} *eastwood-version*]
