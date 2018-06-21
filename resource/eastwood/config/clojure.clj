0247a3201907d773c458bbec52dae67628691d54
Camilo Roca <carocad@unal.edu.co>
2018-06-21 12:41:46 +0200

    exclude coll-of spec macro

--- resource/eastwood/config/clojure.clj
+++ resource/eastwood/config/clojure.clj
@@ -37,7 +37,8 @@
    :for-macro 'clojure.core/and
    :if-inside-macroexpansion-of #{'clojure.spec/every 'clojure.spec.alpha/every
                                   'clojure.spec/and 'clojure.spec.alpha/and
-                                  'clojure.spec/keys 'clojure.spec.alpha/keys}
+                                  'clojure.spec/keys 'clojure.spec.alpha/keys
+                                  'clojure.spec/coll-of 'clojure.spec.alpha/coll-of}
    :within-depth 6
    :reason "clojure.spec's macros `keys`, `every`, and `and` often contain `clojure.core/and` invocations with only one argument."})
 
@@ -46,3 +47,9 @@
   :if-inside-macroexpansion-of #{'clojure.core/cond-> 'clojure.core/cond->>}
   :within-depth 2
   :reason "Allow cond-> and cond->> to have constant tests without warning"})
+
+(disable-warning
+  {:linter :constant-test
+   :if-inside-macroexpansion-of #{'clojure.core/as->}
+   :within-depth 2
+   :reason "Allow as-> to have constant tests without warning"})
