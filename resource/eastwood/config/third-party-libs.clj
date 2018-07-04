1f25762bb09cb5c221de7f7f8aece7807d8305cf
Erik Assum <erik@assum.net>
2018-07-04 23:28:44 +0200

    Updated nestedness as suggested by @psalaberria002

--- resource/eastwood/config/third-party-libs.clj
+++ resource/eastwood/config/third-party-libs.clj
@@ -301,5 +301,5 @@
  {:linter :suspicious-expression
   :for-macro 'clojure.core/and
   :if-inside-macroexpansion-of #{'slingshot.slingshot/try+}
-  :within-depth 10
+  :within-depth 15
   :reason "slingshot.slingshot/try+ generates an (and ...) with one or more arguments. Suppress the 'and called with 1 args' warning."})
