80859d9e17c013783374e04f7b1f4188c3734eba
Erik Assum <erik@assum.net>
2018-10-03 21:30:48 +0200

    Version 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonase/eastwood "0.3.0"
+(defproject jonase/eastwood "0.3.0-SNAPSHOT"
   :description "A Clojure lint tool"
   :url "https://github.com/jonase/eastwood"
   :license {:name "Eclipse Public License"
