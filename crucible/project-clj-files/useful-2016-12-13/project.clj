d000f26bc2163bf95bd9bd778f58654e09c3b3df
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-08 11:46:09 -0700

    Update useful project in crucible to latest version
    Also avoid linting some of its namespaces that cause troubles, either
    for Eastwood, or with recent JDK versions.

--- crucible/project-clj-files/useful-2016-12-13/project.clj
+++ crucible/project-clj-files/useful-2016-12-13/project.clj
@@ -1,19 +1,19 @@
-(defproject org.flatland/useful "0.11.3"
+(defproject org.flatland/useful "0.11.5"
   :description "A collection of generally-useful Clojure utility functions"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :url "https://github.com/flatland/useful"
+  :url "https://github.com/amalloy/useful"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/tools.reader "0.7.2"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default" "test"]}
-  :global-vars {*warn-on-reflection* true}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.6,default:dev,1.7,default:dev,1.8,default:dev,1.9,default:dev,1.10,default" "test"]}
+  :profiles {:1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]} }
   :plugins [[codox "0.8.0"]]
