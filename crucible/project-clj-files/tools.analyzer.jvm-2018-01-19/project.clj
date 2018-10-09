42be9fef07a57ba83c2f9351003cd0ef380d917c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 02:31:18 -0700

    Update tools.analyzer.jvm in crucible to latest version

--- crucible/project-clj-files/tools.analyzer.jvm-2018-01-19/project.clj
+++ crucible/project-clj-files/tools.analyzer.jvm-2018-01-19/project.clj
@@ -1,13 +1,13 @@
-(defproject org.clojure/tools.analyzer.jvm "0.7.1-SNAPSHOT"
+(defproject org.clojure/tools.analyzer.jvm "0.7.3-SNAPSHOT"
   :description "Additional jvm-specific passes for tools.analyzer."
   :url "https://github.com/clojure/tools.analyzer.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.memoize "0.5.9"]
-                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [org.clojure/tools.reader "1.1.1"]
                  [org.clojure/tools.analyzer "0.6.9"]
                  [org.ow2.asm/asm "5.2"]]
   :global-vars {*warn-on-reflection* true}
@@ -17,5 +17,5 @@
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
              }
-  :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
-                 ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]])
+  :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/releases"]
+                 ["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
