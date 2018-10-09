60e4fa6eb519a062305d841a398c9263a1fb1596
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 03:02:49 -0700

    Update java.data in crucible to latest version

--- crucible/project-clj-files/java.data-2018-06-05/project.clj
+++ crucible/project-clj-files/java.data-2018-06-05/project.clj
@@ -1,13 +1,9 @@
-(defproject org.clojure/algo.generic "0.1.2-SNAPSHOT"
-  :description "algo.generic 0.1.2-SNAPSHOT"
+(defproject org.clojure/java.data "0.1.2-SNAPSHOT"
+  :description "java.data 0.1.2-SNAPSHOT"
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.3"]]
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
