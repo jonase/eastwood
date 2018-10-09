84f9f770135ad34770768dee0934fbcec4ac035d
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 00:54:13 -0700

    Updated data.avl in crucible to latest version

--- crucible/project-clj-files/data.avl-2018-10-08/project.clj
+++ crucible/project-clj-files/data.avl-2018-10-08/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.6.1"
   :parent [org.clojure/pom.contrib "0.1.2"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :jvm-opts ^:replace ["-Dorg.clojure.data.avl.test.large-tree-size=100000"
                        "-Dorg.clojure.data.avl.test.medium-tree-size=100000"
                        "-Dorg.clojure.data.avl.test.small-tree-size=300"]
