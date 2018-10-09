eeabbff40882a16b2cea12125a2ce0fd7a77c199
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 02:21:58 -0700

    Update tools.analyzer in crucible to latest version

--- crucible/project-clj-files/tools.analyzer-2018-01-19/project.clj
+++ crucible/project-clj-files/tools.analyzer-2018-01-19/project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.datomic/datomic-free "0.9.5327" :scope "provided" :exclusions [joda-time]]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
              }
-  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
-                 [com.datomic/datomic-free "0.9.5327" :scope "provided" :exclusions [joda-time]]]
-  :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
-                 ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]])
+  :global-vars {*warn-on-reflection* true}
+  :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/releases"]
+                 ["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
