5dccf8879021a78cf02eb2c88b52ad0cca8edb5c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 01:19:05 -0700

    Update tools.cli in crucible to latest version

--- crucible/project-clj-files/tools.cli-2018-10-08/project.clj
+++ crucible/project-clj-files/tools.cli-2018-10-08/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.cli "0.3.2-SNAPSHOT"
+(defproject org.clojure/tools.cli "0.4.2-SNAPSHOT"
   :description "Command line arguments library."
   :parent [org.clojure/pom.contrib "0.1.2"]
   :url "https://github.com/clojure/tools.cli"
@@ -8,14 +8,8 @@
   :test-paths ["src/test/clojure"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
 
@@ -34,6 +28,6 @@
                                                             :runner
                                                             "target/cli_test.js"]}}}}
 
-  :aliases {"test-all" ["with-profile" "test,1.2:test,1.3:test,1.4:test,1.5:test,1.6" "test"]
-            "check-all" ["with-profile" "1.2:1.3:1.4:1.5:1.6" "check"]}
+  :aliases {"test-all" ["with-profile" "test,1.8:test,1.9:test,1.10" "test"]
+            "check-all" ["with-profile" "1.8:1.9:1.10" "check"]}
   :min-lein-version "2.0.0")
