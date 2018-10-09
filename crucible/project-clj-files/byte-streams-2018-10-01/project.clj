c73d1fdb5562e5b12372a54e90323a8f8e8e11da
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 04:19:49 -0700

    Update byte-streams in crucible to latest version

--- crucible/project-clj-files/byte-streams-2018-10-01/project.clj
+++ crucible/project-clj-files/byte-streams-2018-10-01/project.clj
@@ -1,12 +1,13 @@
-(defproject byte-streams "0.2.4-alpha3"
+(defproject byte-streams "0.2.5-alpha2"
   :description "A simple way to handle the menagerie of Java byte represenations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[primitive-math "0.1.6"]
                  [clj-tuple "0.2.2"]
-                 [manifold "0.1.7-alpha5"]]
+                 [manifold "0.1.8"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/test.check "0.9.0"]
+                                  [rhizome "0.2.9"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
