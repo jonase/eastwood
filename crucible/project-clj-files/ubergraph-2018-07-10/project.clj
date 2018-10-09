0a87200b79c3a2b6d8816a65b0d2d5af7babca90
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 01:43:08 -0700

    Add most recent version of ubergraph as new crucible project

---
+++ crucible/project-clj-files/ubergraph-2018-07-10/project.clj
@@ -0,0 +1,18 @@
+(defproject ubergraph "0.5.1"
+  :description "Feature-loaded graph implementation"
+  :url "http://github.com/engelberg/ubergraph"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.priority-map "0.0.10"]
+                 [aysylu/loom "1.0.1"]
+                 [dorothy "0.0.6"]
+                 [potemkin "0.4.3"]]
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
+             }
+  :codox {:output-path "doc"
+          :namespaces [ubergraph.core ubergraph.alg]
+          :source-uri "http://github.com/Engelberg/ubergraph/tree/master/{filepath}#L{line}"}
+  )
