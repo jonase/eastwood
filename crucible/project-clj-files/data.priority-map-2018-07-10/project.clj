7668395b86b1d49d80fb9b14210d3e51a32481a0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-09 01:53:34 -0700

    Update data.priority-map in crucible to latest version

---
+++ crucible/project-clj-files/data.priority-map-2018-07-10/project.clj
@@ -0,0 +1,19 @@
+(defproject org.clojure/data.priority-map "0.0.11-SNAPSHOT"
+  ;;:description "clojure.data.priority-map 0.0.11-SNAPSHOT"
+  ;;:main clojure.data.priority-map
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 ;;[org.clojure/clojure "1.6.0-master-SNAPSHOT"]
+                ]
+  :profiles {:1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-master-SNAPSHOT"]]}
+             }
+  :aliases {"test-all" ["with-profile" "test,1.6:test,1.7:test,1.8:test,1.9:test,1.10" "test"]
+            "check-all" ["with-profile" "1.6:1.7:1.8:1.9:1.10" "check"]}
+  :source-paths [ "src/main/clojure" ]
+  :test-paths [ "src/test/clojure" ]
+  )
