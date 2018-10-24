6a9846df4ecd9daa129ed9b809938e6e585c33bc
Erik Assum <erik@assum.net>
2018-10-24 21:41:34 +0200

    Preparing for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonase/eastwood "0.3.2-SNAPSHOT"
+(defproject jonase/eastwood "0.3.2"
   :description "A Clojure lint tool"
   :url "https://github.com/jonase/eastwood"
   :license {:name "Eclipse Public License"
