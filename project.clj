3040dec456fa94861246bd05d8c9dc0e468bd469
Erik Assum <erik@assum.net>
2020-12-07 20:45:10 +0100

    Release 0.3.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jonase/eastwood "0.3.11"
+(defproject jonase/eastwood "0.3.12"
   :description "A Clojure lint tool"
   :url "https://github.com/jonase/eastwood"
   :license {:name "Eclipse Public License"
