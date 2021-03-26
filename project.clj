698d4be0a5fbce0f63b578cead72559a17a17b0b
vemv <vemv@users.noreply.github.com>
2021-03-26 20:48:57 +0100

    Move `leiningen.eastwood` to its own source path
    This way one can use tools.namespace `(refresh)` without needing the leiningen-core dependency to be present (which tends to be a bad practice in non-plugins)

--- project.clj
+++ project.clj
@@ -1,14 +1,21 @@
+(def eval-in-leiningen?
+  (#{"1" "true"} (System/getenv "EVAL_IN_LEININGEN")))
+
+(def plugin-source-path "lein-eastwood")
+
 (defproject jonase/eastwood "0.3.14"
   :description "A Clojure lint tool"
   :url "https://github.com/jonase/eastwood"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src" "copied-deps"]
+  :source-paths ~(cond-> ["src" "copied-deps"]
+                   eval-in-leiningen? (conj plugin-source-path))
   :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "5.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.5"]
                                   [jafingerhut/dolly "0.1.0"]]}
+             :eastwood-plugin {:source-paths [~plugin-source-path]}
              :warn-on-reflection {:global-vars {*warn-on-reflection* true}}
              :test {:dependencies [[commons-io "2.4" #_"Needed for issue-173-test"]]
                     :resource-paths ["test-resources"
@@ -31,18 +38,10 @@
              :debug #{}}
   :plugins [[net.assum/lein-ver "1.2.0"]]
   :lein-ver {:version-file "resources/EASTWOOD_VERSION"}
-  ;; Note: comment out the following line if you want to do 'lein
-  ;; test' and get a Clojure version later than 1.5.1, even if you
-  ;; have an explicit org.clojure/clojure in the :dependencies
-  ;; Also: Having the following line uncommented causes a problem when
-  ;; doing 'lein test', I think because of a different version of
-  ;; core.cache or core.memoize on which the latest tools.analyzer.jvm
-  ;; 0.1.0-SNAPSHOT depends, versus what Leiningen itself probably
-  ;; depends on.
-
-  ;;  :eval-in-leiningen true
-
   ;; Eastwood may work with earlier Leiningen versions, but this is
   ;; close to the earliest version that it was most tested with.
   :min-lein-version "2.3.0"
-  :resource-paths ["resource" "resources"])
+  :resource-paths ["resource" "resources"]
+  :eval-in ~(if eval-in-leiningen?
+              :leiningen
+              :subprocess))
