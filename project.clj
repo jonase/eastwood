50e0e737498afb82c13713885eed74d50443b622
vemv <vemv@users.noreply.github.com>
2021-03-26 20:35:47 +0100

    Make the test suite pass without -dev
    And particularly, without the `leiningen-core` dependency which makes it plausible that the test suite could be passing for fragile reasons.

--- project.clj
+++ project.clj
@@ -7,45 +7,24 @@
                 ;;*unchecked-math* :warn-on-boxed
                 }
   :source-paths ["src" "copied-deps"]
-  :dependencies [
-                 ;[org.clojure/clojure "1.5.1"]
-                 ;[org.clojure/clojure "1.6.0"]
-                 ;[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojure "1.8.0"]
-                 ;[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
                  [org.ow2.asm/asm-all "5.2"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.macro "0.1.5"]
-                                  [jafingerhut/dolly "0.1.0"]
-                                  [leiningen-core "2.7.1"]]
-                   ;; I want the namespaces defined in files in
-                   ;; "cases" and its subdirectories to be accessible
-                   ;; during 'lein test' in the classpath, but they
-                   ;; themselves should not be eval'd by 'lein test'.
-                   ;; I also want those namespaces available for
-                   ;; one-off tests at the command line like "lein
-                   ;; eastwood '{:namespaces [testcases.testtest]}'",
-                   ;; and for that, the :test profile does not work,
-                   ;; so put them in :dev
-                   :resource-paths [ "cases" ]}
-             :test {:dependencies [
-                                   ;; Needed for issue-173-test
-                                   [commons-io "2.4"]
-                                   ]
-                    :resource-paths ["test-resources"]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+                                  [jafingerhut/dolly "0.1.0"]]}
+             :test {:dependencies [[commons-io "2.4" #_"Needed for issue-173-test"]]
+                    :resource-paths ["test-resources"
+                                     ;; if wanting the `cases` to be available during development / the default profile,
+                                     ;; please simply add `with-profile +test` to your CLI invocation.
+                                     "cases"]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :1.10.2 {:dependencies [[org.clojure/clojure "1.10.2"]]}}
   :aliases {"test-all" ["with-profile"
-                        "dev,test,1.7:dev,test,1.8:dev,test,1.9:dev,test,1.10.1:dev,test,1.10.2"
-                        "test"]
-            "lint-all" ["with-profile"
-                        "dev,test,1.7:dev,test,1.8:dev,test,1.9:dev,test,1.10.1:dev,test,1.10.2"
-                        "eastwood-all"]}
+                        "-dev,+test,+1.7:-dev,+test,+1.8:-dev,+test,+1.9:-dev,+test,+1.10"
+                        "test"]}
   :eastwood {:source-paths ["src"]
              :test-paths ["test"]
              :only-modified true
