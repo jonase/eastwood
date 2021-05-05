f7d7407879c7bc48acb7d8b27612880cdd7b00d9
Erik Assum <erik.assum@ardoq.com>
2021-05-05 22:23:03 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 
 (def plugin-source-path "lein-eastwood")
 
-(defproject jonase/eastwood "0.4.2"
+(defproject jonase/eastwood "0.4.2-SNAPSHOT"
   :description "A Clojure lint tool"
   :url "https://github.com/jonase/eastwood"
   :license {:name "Eclipse Public License"
