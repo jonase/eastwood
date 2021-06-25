ea61296880e36265dbb7e9bf682f79ac6f79ea4b
vemv <vemv@users.noreply.github.com>
2021-06-25 16:48:51 +0200

    Exercise Eastwood against Crux in CI

---
+++ .circleci/crucible.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -Euxo pipefail
+
+lein with-profile -user,-dev,+eastwood-plugin install || exit 1
+
+cd .circleci || exit 1
+
+git submodule update --init --recursive
+
+cd ./core.async || exit 1
+
+if lein with-profile +test update-in :plugins conj "[jonase/eastwood \"RELEASE\"]" -- eastwood | tee output; then
+  echo "Should have failed! Emitted output:"
+  cat output
+  exit 1
+fi
+
+grep --silent "== Warnings: 39 (not including reflection warnings)  Exceptions thrown: 0" output  || exit 1
+
+cd ../crux || exit 1
+./lein-sub install
+./lein-sub with-profile -user,+test update-in :dependencies conj "[jonase/eastwood \"RELEASE\"]" -- run -m eastwood.lint "{:source-paths [\"src\"], :test-paths [\"test\"], :forced-exit-code 0}" | tee output
+
+# Assert that no exceptions are thrown, and that useful insights are found:
+ex_marker=" Exceptions thrown:"
+grep --silent "Warnings: 1 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 2 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 3 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 4 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 16 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 18 (not including reflection warnings) $ex_marker 0" output  || exit 1
+grep --silent "Warnings: 42 (not including reflection warnings) $ex_marker 0" output  || exit 1
+if grep --silent "$ex_marker [1-9]" output; then
+  echo "Should not have thrown an exception!"
+  exit 1
+fi
+
+zero_warns=$(grep -c "Warnings: 0 (not including reflection warnings) $ex_marker 0" output)
+if [ "$zero_warns" != "10" ]; then
+  exit 1
+fi
+
+exit 0
