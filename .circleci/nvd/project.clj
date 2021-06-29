f2bd347d6cb99483ef8ea3800c94fc0378805f23
vemv <vemv@users.noreply.github.com>
2021-06-29 14:15:42 +0200

    CI: cache NVD databases for up to 24h
    This should speed up the build and avoid spamming the NVD servers (which can be flaky anyway).

--- .circleci/nvd/project.clj
+++ .circleci/nvd/project.clj
@@ -2,7 +2,9 @@
   :description "Meant for analyzing other projects' classpaths in an isolated manner,
 so that lein-nvd's dependencies don't affect the analyzed corpus (leading to false positives/negatives) OR
 the target project's dependencies affect lein-nvd's implementation (leading to bugs)."
-  :dependencies [[nvd-clojure "1.5.0"]
+  :dependencies [ ;; if updating nvd-clojure, please make sure that the `~/.m2/repository/org/owasp/dependency-check-utils/6.2.2/data` key remains valid
+                 ;; in .circleci/config.yml (as that transitive dependency might change):
+                 [nvd-clojure "1.5.0"]
                  [org.clojure/clojure "1.10.3"]]
   :jvm-opts ["-Dclojure.main.report=stderr"]
   :pedantic? :abort)
