95d08ea206f1b4e2a3414327ded297728c5d26a3
vemv <vemv@users.noreply.github.com>
2022-01-08 18:45:46 +0100

    Revamp nvd setup

--- .circleci/nvd/run.sh
+++ .circleci/nvd/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
-NVD_DIR=".circleci/nvd"
-PROJECT_CLASSPATH="$(LEIN_SILENT=true lein with-profile -user,-dev classpath)"
+NVD_DIR="$PWD/.circleci/nvd"
+# calculate a production classpath within the main project:
+CLASSPATH="$(LEIN_SILENT=true lein with-profile -user,-dev classpath)"
 
-# Inspired by https://github.com/rm-hull/lein-nvd/tree/b455ec33baa837a4e11031985251ca81d5ceb86f#avoiding-classpath-interference
-DEPENDENCY_CHECK_COMMAND="lein with-profile -user,-dev run -m nvd.task.check $PWD/$NVD_DIR/nvd-config.json $PROJECT_CLASSPATH"
+DEPENDENCY_CHECK_COMMAND="clojure -M:nvd $NVD_DIR/nvd-config.json $CLASSPATH"
 
+# cd into a different project defining its own deps.edn:
 cd "$NVD_DIR" || exit 1
 
 # Try a few times in face of flakiness:
