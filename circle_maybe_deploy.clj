f202593823a5904ce3e31c85e4a30e031079325b
vemv <vemv@users.noreply.github.com>
2022-01-11 17:19:25 +0100

    Revamp deploy script

---
+++ circle_maybe_deploy.clj
@@ -0,0 +1,28 @@
+(ns circle-maybe-deploy
+  (:require
+   [clojure.java.shell :refer [sh]]
+   [clojure.string :as str]))
+
+(def release-marker "v")
+
+(defn make-version! [tag]
+  (str/replace-first tag release-marker ""))
+
+(defn log-result [m]
+  (println m)
+  m)
+
+(defn -main [& _]
+  (let [tag (System/getenv "CIRCLE_TAG")]
+    (if-not tag
+      (System/exit 1)
+      (if-not (re-find (re-pattern release-marker) tag)
+        (System/exit 1)
+        (do
+          (apply println "Executing" *command-line-args*)
+          (->> [:env (into {"PROJECT_VERSION" (make-version! tag)} (System/getenv))]
+               (into (vec *command-line-args*))
+               (apply sh)
+               log-result
+               :exit
+               (System/exit)))))))
